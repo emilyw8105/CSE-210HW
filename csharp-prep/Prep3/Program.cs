b28ae21b5dd271c7520c8c11212bfdc6ec0f9cd6
Emily White <emilyw8105@gmail.com>
2025-09-25 13:27:49 -0600

    Changed template to final program

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,33 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        
+        int Guess_Number = 0;
+
+        //Console.Write("What is the magic number? ");
+        //Magic_Number = int.Parse(Console.ReadLine());
+        Random Rando_Magic_Number = new Random();
+        int Magic_Number = Rando_Magic_Number.Next(1 , 100);
+
+        while (Guess_Number != Magic_Number)
+        {
+
+
+            Console.Write("What is your guess? ");
+            Guess_Number = int.Parse(Console.ReadLine());
+
+            if (Guess_Number > Magic_Number)
+            {
+                Console.Write("Lower \n");
+            }
+            if (Guess_Number < Magic_Number)
+            {
+                Console.Write("Higher \n");
+            }
+            if (Guess_Number == Magic_Number)
+            {
+                Console.Write("You guessed it!");
+            }
+        }
     }
 }
