10f803cbede1305152ed93e0ecc3d53397f2c236
Emily White <emilyw8105@gmail.com>
2025-10-09 13:27:06 -0600

    Changed code to fit Learning Activity Requirements

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,26 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job Job1 = new Job();
+        Job1._JobTitle = "Software Engineer";
+        Job1._Company = "Microsoft";
+        Job1._StartYear = 2019;
+        Job1._EndYear = 2022;
+
+        Job Job2 = new Job();
+        Job2._JobTitle = "Manager";
+        Job2._Company = "Apple";
+        Job2._StartYear = 2022;
+        Job2._EndYear = 2023;
+
+        
+
+        Resume myResume = new Resume();
+        myResume._Name = "Allison Rose";
+
+        myResume._Jobs.Add(Job1);
+        myResume._Jobs.Add(Job2);
+
+        myResume.Display();
     }
 }
